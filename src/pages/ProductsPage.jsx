87f2fff186c7cbb92d66d76a0a2daf04d8b8d73a
prachi-stack <deopaprachi60@gmail.com>
2025-01-03 10:41:31 +0530

    Update ProductsPage.jsx

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -22,7 +22,7 @@ const ProductsPage = () => {
 					<StatCard name='Total Products' icon={Package} value={1234} color='#6366F1' />
 					<StatCard name='Top Selling' icon={TrendingUp} value={89} color='#10B981' />
 					<StatCard name='Low Stock' icon={AlertTriangle} value={23} color='#F59E0B' />
-					<StatCard name='Total Revenue' icon={DollarSign} value={"$543,210"} color='#EF4444' />
+					<StatCard name='Total Revenue' icon={DollarSign} value={"Rs.543,210"} color='#EF4444' />
 				</motion.div>
 
 				<AvailableProducts />
